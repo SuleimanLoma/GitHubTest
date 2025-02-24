247e2fe2702a429726686ab148a2960b531fc14b
Suleiman Loma <suleimanloma@gmail.com>
2025-02-24 16:24:34 +0600

    added project

---
+++ Lection 15 Alerts/Lection 15 Alerts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lection 15 Alerts
+//
+//  Created by Suleiman Loma on 24/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lection 15 Alerts/Lection 15 Alerts/Lection_15_AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lection_15_AlertsApp.swift
+//  Lection 15 Alerts
+//
+//  Created by Suleiman Loma on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Lection_15_AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
