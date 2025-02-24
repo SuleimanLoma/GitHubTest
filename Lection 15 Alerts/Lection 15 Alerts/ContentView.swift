ac8b19f33fa1c4ddb63ab4ed2d6557568972df7e
Suleiman Loma <suleimanloma@gmail.com>
2025-02-24 19:16:55 +0600

    Test2

--- Lection 15 Alerts/Lection 15 Alerts/ContentView.swift
+++ Lection 15 Alerts/Lection 15 Alerts/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "cloud.sun.fill")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.blue)
+                Text("Hello")
+                
+                
         }
-        .padding()
     }
 }
 
